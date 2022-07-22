9e5e331c3d8e7b9020d2dfb012014b9f85d0beb4
vosooghifarshid95@gmail.com <vosooghifarshid95@gmail.com>
2022-07-22 15:30:33 +0430

    last commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new Employee();
-        System.out.println(employee.name);
-        System.out.println(employee.family);
-        System.out.print("1")
+        System.out.print("1");
         System.out.println("Hi!");
     }
 }
