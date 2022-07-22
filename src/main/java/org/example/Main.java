57e86c0c333fb85f0e32b1be4190a527b245a7d0
vosooghifarshid95@gmail.com <vosooghifarshid95@gmail.com>
2022-07-22 14:56:28 +0430

    Hi!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee();
         System.out.println(employee.name);
+        System.out.println("Hi!");
     }
 }
